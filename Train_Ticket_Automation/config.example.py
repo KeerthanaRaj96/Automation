0ec73ac1c9d6f1591f7fa8309cd55268fe64c770
KeerthanaRaj96 <keerthanar9622@gmail.com>
2026-05-25 15:27:50 +0530

    Create config.example.py

---
+++ Train_Ticket_Automation/config.example.py
@@ -0,0 +1,9 @@
+USERNAME = "your_irctc_username"
+PASSWORD = "your_irctc_password"
+
+FROM_STATION = "SBC"
+TO_STATION = "MAS"
+DATE = "DD/MM/YYYY"
+
+PASSENGER_NAME = "Your Name"
+PASSENGER_AGE = "00"
